382549684065bbfcb9240b7d4af2f74851a70bb7
Safwan-debug <78640149+Safwan-debug@users.noreply.github.com>
2023-10-15 22:03:16 +0530

    new line added

--- mywork.py
+++ mywork.py
@@ -1,1 +1,24 @@
 print('Bismilla')
+
+def saf():
+    n = 10
+    return n
+
+print(saf())
+
+class Mywork:
+    def __init__(self,name,age,branch):
+        self.name = name
+        self.age = age
+        self.branch = branch
+        
+    def b_print_(self):
+        print('My name is :',self.name)
+        print('my age is :',self.age)
+        print('my braanch is :',self.branch)
+        
+    
+
+obj = Mywork("safwan",25,"BE")
+obj.b_print_()
+        
